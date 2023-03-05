a79b2d68425d5b7ab0991d81f9d78da178222a58
Maugli <101056287+DaMaugli@users.noreply.github.com>
2023-03-05 17:57:39 +0200

    Update tbex.js

--- tbex.js
+++ tbex.js
@@ -1,60 +1,72 @@
 (function (ext) {
-  // Define a new block that opens a pop-up window
-  ext.openPopUp = function (url) {
-    window.open(url, '_blank', 'width=500,height=500');
-  };
-
-  // Define a new block that shows a warning dialog
-  ext.showWarningDialog = function (message) {
-    window.alert(message);
-  };
-
-  // Define a new block that moves the window to the specified X and Y coordinates
-  ext.moveWindowTo = function (x, y) {
-    window.moveTo(x, y);
-  };
-
-  // Define a new block that resizes the window to the specified width and height
-  ext.resizeWindow = function (width, height) {
-    window.resizeTo(width, height);
-  };
-
-  // Define a new block that enters fullscreen mode
-  ext.goFullscreen = function() {
-    document.documentElement.requestFullscreen();
-  };
-
-  // Define a new block that exits fullscreen mode
-  ext.exitFullscreen = function() {
-    document.exitFullscreen();
-  };
-
-  // Define a new block that prompts the user with a confirmation message before closing the game
-  ext.closeGameWithConfirmation = function() {
-    if (confirm("Are you sure you want to close the game?")) {
+    // open tab
+    ext.openPopUp = function (url) {
+      window.open(url, '_blank', 'width=500,height=500');
+    };
+  
+    // show warning dialog
+    ext.showWarningDialog = function (message) {
+      window.alert(message);
+    };
+  
+    // move window
+    ext.moveWindowTo = function (x, y) {
+      window.moveTo(x, y);
+    };
+  
+    // resize window
+    ext.resizeWindow = function (width, height) {
+      window.resizeTo(width, height);
+    };
+  
+    // fullscreen
+    ext.goFullscreen = function() {
+      document.documentElement.requestFullscreen();
+    };
+  
+    // exit fullscreen
+    ext.exitFullscreen = function() {
+      document.exitFullscreen();
+    };
+  
+    // close with are u sure
+    ext.closeGameWithConfirmation = function() {
+      if (confirm("Are you sure you want to close the game?")) {
+        window.close();
+      }
+    };
+  
+    // crash game
+    ext.closeGameWithoutConfirmation = function() {
       window.close();
-    }
-  };
-
-  // Define a new block that closes the game without any confirmation prompt
-  ext.closeGameWithoutConfirmation = function() {
-    window.close();
-  };
-
-  // Define the descriptor for the new category of blocks
-  var descriptor = {
-    blocks: [
-      [' ', 'open browser tab with URL %s', 'openPopUp', 'https://example.com'],
-      [' ', 'show warning dialog with message %s', 'showWarningDialog', 'This is a warning message.'],
-      [' ', 'move window to X: %n Y: %n', 'moveWindowTo', 0, 0],
-      [' ', 'resize window to width %n height %n', 'resizeWindow', '500', '500'],
-      [' ', 'fullscreen', 'goFullscreen'],
-      [' ', 'exit fullscreen', 'exitFullscreen'],
-      [' ', 'close game with confirmation', 'closeGameWithConfirmation'],
-      [' ', 'close game without confirmation(crash)', 'closeGameWithoutConfirmation']
-    ],
-  };
-
-  // Register the extension with ScratchExtensions
-  ScratchExtensions.register("maugli's windows tools", descriptor, ext);
-})({});
+    };
+  
+    // get screen resolution
+    ext.getScreenWidth = function() {
+      return window.screen.width;
+    };
+  
+    ext.getScreenHeight = function() {
+      return window.screen.height;
+    };
+  
+    // put blocks here
+    var descriptor = {
+      blocks: [
+        [' ', 'open browser tab with URL %s', 'openPopUp', 'https://example.com'],
+        [' ', 'show warning dialog with message %s', 'showWarningDialog', 'This is a warning message.'],
+        [' ', 'move window to X: %n Y: %n', 'moveWindowTo', 0, 0],
+        [' ', 'resize window to width %n height %n', 'resizeWindow', '500', '500'],
+        ['r', 'screen width:', 'getScreenWidth'],
+        ['r', 'screen height:', 'getScreenHeight'],
+        [' ', 'fullscreen', 'goFullscreen'],
+        [' ', 'exit fullscreen', 'exitFullscreen'],
+        [' ', 'close game with confirmation', 'closeGameWithConfirmation'],
+        [' ', 'close game without confirmation(crash)', 'closeGameWithoutConfirmation']
+      ],
+    };
+  
+    // this important thing lmao
+    ScratchExtensions.register("maugli's windows tools", descriptor, ext);
+  })({});
+  
